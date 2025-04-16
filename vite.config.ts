ba7e15661f2df7eafb47b841b490e3f2dcca0c43
lkogomes <luan.gomes@sead.se.gov.br>
2025-04-16 09:33:15 -0300

    Up

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   base: '/Calculadora-de-Horas-Trabalhadas/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [react()],
 });
